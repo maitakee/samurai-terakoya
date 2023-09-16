1630c092017048ddd26a3425d5133bc7455b5b5f
maitakee <136719899+maitakee@users.noreply.github.com>
2023-09-16 20:03:31 +0900

    kadai_021

---
+++ confirmation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+  <meta charset='UTF-8'>
+  <title>PHP kadai_021確認画面</title>
+</head>
+
+<body>
+  <h2>入力内容をご確認ください。</h2>
+  <p>お名前:<?php echo $_POST['user_name']; ?></p>
+  <p>性別:<?php echo $_POST['user_gender']; ?></p>
+  <p>メールアドレス:<?php echo $_POST['user_email'] ?></p>
+</body>
+</html>
